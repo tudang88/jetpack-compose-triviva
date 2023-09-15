f447b1f1f7a52dad3c6acb3c8aebd8e9a035c485
dangtu <dang.huu.tu88@gmail.com>
2023-09-15 23:18:11 +0900

    Add appbar to HomeScreen

--- app/src/main/java/com/composebootcamp/triviva/screens/HomeScreen.kt
+++ app/src/main/java/com/composebootcamp/triviva/screens/HomeScreen.kt
@@ -1,12 +1,24 @@
 package com.composebootcamp.triviva.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,28 +32,53 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.composebootcamp.triviva.R
+import com.composebootcamp.triviva.ui.theme.ButtonPlayBgColor
+import com.composebootcamp.triviva.ui.theme.ButtonPlayCaptionColor
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavController?) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceEvenly
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.android_trivia),
-            contentDescription = "home image",
-            modifier = Modifier.padding(all = 24.dp)
+    Scaffold(topBar = {
+        CenterAlignedTopAppBar(
+            colors = TopAppBarDefaults.smallTopAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.primary,
+            ),
+            title = {
+                Text(stringResource(id = R.string.android_trivia))
+            },
+            navigationIcon = {
+                IconButton(onClick = {/* todo */}) {
+                    Icon(imageVector = Icons.Filled.Menu,
+                    contentDescription = "Navigation Drawer Button")
+                }
+            }
         )
-        Button(shape = RectangleShape,
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFEADBC8)),
-            onClick = { /*TODO*/ }) {
-            Text(
-                text = stringResource(id = R.string.play_button_text),
-                style = TextStyle(color = Color(0xFFBC7AF9), fontWeight = FontWeight.Bold)
+    }) { padding ->
+        Column(
+            modifier = Modifier.padding(paddingValues = padding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.android_trivia),
+                contentDescription = "home image",
+                modifier = Modifier.padding(all = 24.dp)
             )
+            Button(shape = RectangleShape,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = ButtonPlayBgColor
+                ),
+                onClick = { /*TODO*/ }) {
+                Text(
+                    text = stringResource(id = R.string.play_button_text),
+                    style = TextStyle(color = ButtonPlayCaptionColor, fontWeight = FontWeight.Bold)
+                )
+            }
         }
     }
+
 }
 
 @Preview(showBackground = true)
