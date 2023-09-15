765a0d96b4ead3c79dbf999c9ac1eb6ca125dbfc
dangtu <dang.huu.tu88@gmail.com>
2023-09-15 22:22:21 +0900

    implement HomeScreen

--- app/src/main/java/com/composebootcamp/triviva/HomeScreen.kt
+++ app/src/main/java/com/composebootcamp/triviva/HomeScreen.kt
@@ -1,10 +1,50 @@
 package com.composebootcamp.triviva
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 
 @Composable
-fun HomeScreen(navController: NavController) {
-    
+fun HomeScreen(navController: NavController?) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.android_trivia),
+            contentDescription = "home image",
+            modifier = Modifier.padding(all = 24.dp)
+        )
+        Button(shape = RectangleShape,
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFEADBC8)),
+            onClick = { /*TODO*/ }) {
+            Text(
+                text = stringResource(id = R.string.play_button_text),
+                style = TextStyle(color = Color(0xFFBC7AF9), fontWeight = FontWeight.Bold)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen(navController = null)
 }
--- app/src/main/java/com/composebootcamp/triviva/MainActivity.kt
+++ app/src/main/java/com/composebootcamp/triviva/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.composebootcamp.triviva.navigation.AppNavigation
 import com.composebootcamp.triviva.ui.theme.AndroidTrivivaTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +23,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    AppNavigation()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidTrivivaTheme {
-        Greeting("Android")
     }
 }
