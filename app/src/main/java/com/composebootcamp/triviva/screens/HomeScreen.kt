803f57f9be629d2ca6cd94ea56dd336adb31ea4b
dangtu <dang.huu.tu88@gmail.com>
2023-09-24 04:56:22 +0900

    revise Home column arrangement

--- app/src/main/java/com/composebootcamp/triviva/screens/HomeScreen.kt
+++ app/src/main/java/com/composebootcamp/triviva/screens/HomeScreen.kt
@@ -1,10 +1,9 @@
 package com.composebootcamp.triviva.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
@@ -17,12 +16,10 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -57,7 +54,7 @@ fun HomeScreen(navController: NavController?) {
         )
     }) { padding ->
         Column(
-            modifier = Modifier.padding(paddingValues = padding),
+            modifier = Modifier.padding(paddingValues = padding).fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.SpaceEvenly
         ) {
